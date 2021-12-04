107537e2155b9994d034f97eb379432c0727f903
Tomas Harkema <tomas@harkema.io>
2021-12-04 12:50:37 +0100

    feature/no-ssl

--- Package.swift
+++ Package.swift
@@ -11,7 +11,6 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.33.0"),
-        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.14.0"),
         .package(url: "https://github.com/apple/swift-nio-transport-services.git", from: "1.6.0"),
     ],
     targets: [
@@ -21,7 +20,6 @@ let package = Package(
             .product(name: "NIOConcurrencyHelpers", package: "swift-nio"),
             .product(name: "NIOHTTP1", package: "swift-nio"),
             .product(name: "NIOWebSocket", package: "swift-nio"),
-            .product(name: "NIOSSL", package: "swift-nio-ssl", condition: .when(platforms: [.linux, .macOS])),
             .product(name: "NIOTransportServices", package: "swift-nio-transport-services"),
         ]),
         .testTarget(name: "MQTTNIOTests", dependencies: ["MQTTNIO"]),
